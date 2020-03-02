c11e1e2da8cf58e09a89ad7624f0c0622102fb46
Ben <benjamin.thomas1@viacesi.fr>
2020-03-02 11:36:09 +0100

    Page de connexion , logout, signin

--- login/traitement.php
+++ login/traitement.php
@@ -1,125 +1,47 @@
 <?php
-// Constante DEBUG :
-// true : affiche des infos à des fins de debuggage
-// false : fonctionnement normal
-define("DEBUG", true);
 
-// On teste d'abord la présence des champs du formulaire
-if (!checkInputs()) {
-    checkInputErreur(); // Redirige vers la page d'erreur
-    exit();     // Termine le script (important !)
-                // Sans exit(), PHP continue d'interpréter le script...
-                // et testera donc le user/pass
-                // On aurait aussi pu le mettre dans la fonction checkInputErreur().
-} else {
-    $login = $_POST['userName'];
-    $pass = $_POST['password'];
-    $authentified = false;
-    try {
-        // On se connecte à MySQL >> base Cours_PHP User 'root' mdp 'root'
-        $dbHost = 'localhost';
-        $dbName = 'Cours_PHP';        
-        $dbUser = 'root';
-        $dbPass = '';
-        $dbCnx = "mysql:host=$dbHost;dbname=$dbName;charset=utf8";
-        $bdd = new PDO($dbCnx, $dbUser, $dbPass);
-    } catch (Exception $e) {
-        die('Erreur : '.$e->getMessage());
+  require '../functions.php';
+  session_start();
+
+  //On récupère les champs du formulaires de login:
+  if (isset($_POST['mail']) && isset($_POST['pass'])) {
+
+    $mail = $_POST['mail'];
+    $pass = md5($_POST['pass']);
+
+    //On vérifie qu'aucun champ saisi n'est vide:
+    if (empty($mail) || empty($pass)) {
+      $_SESSION['err_login'] = "Merci de renseigner tous les champs.";
+      header ('Location: login.php');
     }
+    else {
+      //On vérifie en base si le mail renseigné existe :
+      connect_Database();
+      $req = $dbconnect->prepare("SELECT * FROM users WHERE mail = '$mail'");
+      $req -> execute();
+      $count = $req->rowCount();
 
-    debug("login : $login");
-    debug("pass : $pass -> ".md5($pass));
-    
-    $sql = 'select user_password from utilisateur where user_login = :user_login';
-    $stmt = $bdd->prepare($sql);
-    $stmt->bindParam(':user_login', $login);
-    
-    if (!$stmt->execute()) {
-        $err = $stmt->errorInfo();
-        echo "Erreur SQL : <br />";
-        print_r($err);
-        die();
-    }
+      if ($count==0) {
+        $_SESSION['err_login'] = "L'adresse mail renseignée n'est pas enregistrée dans notre base.";
+        header ('Location: login.php');
+      }
+      else {
+        //On vérifie la concordance mail / mdp
+        $req = $dbconnect->prepare("SELECT * FROM users where mail = '$mail' ");
+        $req->execute();
+        $tab = $req->fetch();
 
-    if ($row = $stmt->fetch()) {
-        $passFromDB = $row['user_password'];        
-        debug("Utilisateur trouvé ! Pass from DB = $passFromDB");
-        
-        if ($passFromDB == md5($pass)) {            
-            debug('Pass OK');            
-            $authentified = true;
-            session_start();
-            $_SESSION['user_login']=$login;
-        }  else {            
-            debug('Pass KO');
+        if ($tab['Pass'] != $pass) {
+          $_SESSION['err_login'] = "Le mot de passe saisie n'est pas correct";
+          header ('Location: login.php');
         }
-    } else  {        
-        debug('Utilisateur non trouvé !');        
-    }
-
-    if ($authentified) {
-        checkLoginSucces();
-    } else {                
-        checkLoginErreur();
-    }
-}
-
-/*************************************************************************
- FONCTIONS
-**************************************************************************/
-
-// Fonction de vérification des champs.
-// Retourne true si les champs sont OK, false sinon
-function checkInputs()
-{
-    // Par défaut, le résultat de la fonction sera "true"
-    $result = true;
-
-    $tabloChamp = ["userName", "password"];
-
-    // boucle sur les champs du formulaire.(contrôle de la saisie)
-    foreach ($tabloChamp as $champ) {
-        if (!isset($_POST[$champ]) || empty($_POST[$champ])) {
-            debug("$champ est vide !");
-            
-            // Si le champ n'a pas été transmis, ou est vide,
-            // on passe le résultat à false
-            $result = false;
+        else {
+          $_SESSION['User_Prenom'] = $tab['Prenom'];
+          header ('Location: ../accueil/index.php');
         }
+      }
     }
+  }
 
-    // On retourne le résultat
-    return $result;
-}
 
-function checkInputErreur()
-{
-    if (DEBUG) {
-        die("Ah fug... Input error");
-    } else {
-        header('Location: /skeleton/formulaire/erreur.php');
-    }
-}
-
-function checkLoginErreur()
-{
-    if (DEBUG) {
-        die("Désolé, mais connais pas ! Login error");
-        header('Location: /skeleton/formulaire/erreur.php');
-
-    }
-} 
-
-function checkLoginSucces()
-{
-    if (DEBUG) {
-        // echo "Login: Check !";
-        header('Location: /skeleton/articles/index.php');
-    }
-}
-
-function debug($text) {
-    if (DEBUG) {
-        echo $text."<br />";
-    }
-}
+?>
--- menu.php
+++ menu.php
@@ -68,7 +68,7 @@
 
 <nav class="flex items-center justify-between flex-wrap bg-darkblue-500 p-6">
     <div class="flex items-center flex-shrink-0 text-darkblue mr-6">
-        <img src="logo.png" alt="" width="150" height="">
+        <img src="http://localhost/projet_PHP_Jessy/ProjetPhP/logo.png" alt="" width="150" height="">
         <span class="font-semibold text-xl tracking-tight"></span>
     </div>
     <div class="block lg:hidden">
@@ -87,6 +87,27 @@
             <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-darkblue mr-12">
                 Blog
             </a>
+
+            <?php
+              if (isset($_SESSION['User_Prenom'])){
+                echo $_SESSION['User_Prenom'];
+              }
+              else{
+            ?>
+            <a href="../login/login.php">Se connecter</a>
+            <?php
+              }
+            ?>
+
+            <?php
+              if (isset($_SESSION['User_Prenom'])) {
+            ?>
+            <li> <a href="../logout/logout.php">Deconnexion</a> </li>
+            <?php
+              }
+            ?>
+
+
             <div class="flex flex-row-reverse mr-6 my-2">
                 <div class="relative text-gray-600">
                     <input type="search" name="serch" placeholder="Search" class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none">
@@ -97,92 +118,7 @@
                     </button>
                 </div>
             </div>
-        </div> 
+        </div>
     </div>
 </nav>
 <!-- ---------------------------------------------------------------------------- -->
-<body class="bg-darkblue font-sans leading-normal tracking-normal">
-    <div class="carousel relative shadow-2xl bg-darkblue">
-        <div class="carousel-inner relative overflow-hidden w-full">
-<!--Slide 1-->
-            <input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-            <div class="carousel-item absolute opacity-0" style="height:65vh;"> 
-                <div class="object-cover h-48 w-full text-darkblue text-5xl text-center">
-                    <img src="2.jpg" class="w-m" alt="">
-                </div>
-            </div>
-            <label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-darkblue rounded-full bg-darkblue hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-            <label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-darkblue rounded-full bg-darkblue hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-<!--Slide 2-->
-            <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-                <div class="carousel-item absolute opacity-0" style="height:65vh;">
-                    <div class="block h-full w-full bg-orange-500 text-darkblue text-5xl text-center"></div>
-                </div>
-                <label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-darkblue rounded-full bg-darkblue hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-                <label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-darkblue rounded-full bg-darkblue hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label> 
-
-<!--Slide 3-->
-            <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-                <div class="carousel-item absolute opacity-0" style="height:65vh;">
-                    <div class="block h-full w-full bg-green-500 text-darkblue text-5xl text-center"></div>
-                </div>
-                <label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-darkblue rounded-full bg-darkblue hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-                <label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-darkblue rounded-full bg-darkblue hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-
-<!-- Add additional indicators for each slide-->
-            <ol class="carousel-indicators">
-                <li class="inline-block mr-3">
-                    <label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-darkblue hover:text-blue-700">•</label>
-                </li>
-                <li class="inline-block mr-3">
-                    <label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-darkblue hover:text-blue-700">•</label>
-                </li>
-                <li class="inline-block mr-3">
-                    <label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-darkblue hover:text-blue-700">•</label>
-                </li>
-            </ol>
-        </div>
-    </div>
-<!-- ----------------------------------------------------------- -->
-    <h1> Les recettes du moment</h1>
-    <main class="py-4">
-        <div class="px-4">
-            <div class="block md:flex justify-between md:-mx-2">
-                <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-                    <div class="bg-white rounded-lg overflow-hidden shadow relative">
-                        <img class="h-56 w-full object-cover object-center" src="3.jpg" alt="">          
-                        <div class="p-4 h-auto md:h-40 lg:h-48">
-                            <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
-                                Woman standing under blue sky
-                            </a>
-                            <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem blanditiis unde asperiores? Officia amet perspiciatis ad quibusdam incidunt eaque, nobis, eveniet neque porro id commodi quisquam debitis!
-                            </div>
-                            <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
-                                <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#something</a>
-                                <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#sky</a>
-                            </div>
-                        </div>
-                    </div>  
-                </div>
-                <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
-                    <div class="bg-white rounded-lg overflow-hidden shadow relative">
-                        <img class="h-56 w-full object-cover object-center" src="2.jpg" alt="">          <div class="p-4 h-auto md:h-40 lg:h-48">
-                        <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
-                            Woman walking on pathway
-                        </a>
-                        <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure aut quia alias ullam eveniet sunt! Ipsa, sunt. Inventore ipsum sit quasi. Alias quasi officiis blanditiis!
-                        </div>
-                        <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
-                            <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#pathway</a>
-                            <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#woman</a>
-                            <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#walkalone</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-</body>
