ab438e04ec40fac1f72e9144c9505085d0a218dd
Arnaud Gladieux <arnaud.gladieux@viacesi.fr>
2020-03-03 09:07:04 +0100

    fiche recette

--- ficherecette.php
+++ ficherecette.php
@@ -1,62 +1,83 @@
-<?php session_start() ?>
-<!-- <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-    <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link" href="http://localhost/skeleton/">
-                    Accueil
-                </a>
-            </li>
-        </ul>
-        <ul class="nav flex-column mb-2">
-        <?php if (!isset($_SESSION['user_login'])) : ?>
-            <li class="nav-item">
-                <a class="nav-link" href="http://localhost/skeleton/login/index.php">
-                    Login
-                </a>
-            </li>
-            <?php endif?>
-            <?php if (isset($_SESSION['user_login'])) : ?>
-            <li class="nav-item">
-                <a class="nav-link" href="/skeleton/articles/index.php">
-                    Articles
-                </a>
-            </li>
-            <?php endif?>
-            <?php if (isset($_SESSION['user_login'])) : ?>
-            <li class="nav-item">
-                <a class="nav-link" href="/skeleton/logout/index.php">
-                    Logout
-                </a>
-            </li>
-            <?php endif?> -->
 
 
-
-            <!DOCTYPE html>
+           <?php include __DIR__ . '/header.php' ?> <!DOCTYPE html>
 <html>
+<head>
+</head>
 
 
-            <nav class="flex items-center justify-between flex-wrap bg-darkblue-500 p-6">
-  <div class="flex items-center flex-shrink-0 text-darkblue mr-6">
-    <img src="logo.png" alt="" width="150" height="">
-    <span class="font-semibold text-xl tracking-tight"></span>
-  </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-black-200 border-black-400 hover:text-darkblue hover:border-darkblue">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div>
-  <div class="w-full block flexlg:flex lg:items-center lg:w-auto">
-    <div class="text-sm lg:flex">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-darkblue mr-4">
-        Docs
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-darkblue mr-4">
-        Examples
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:text-darkblue mr-12">
-        Blog
-      </a>
 
-      </html>
+
+
+
+
+
+
+<!--<div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+    <div class="mx-auto px-6 py-4" >
+      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      </p>
+    </div>
+    <div class="px-6 py-4">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    </div>
+  </div>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <!-- <body>
+<div class="flex h-screen">
+  <div class="border-black w-1/5 bg-white-0 h-12"></div>
+  <div class="w-3/5 h-64 bg-gray-500 "> 
+    <div class="w-1/5  bg-white-0 h-12"></div>
+
+</body>
+
+<body>
+    <div class="flex h-64 rounded">
+        <div class="flex-none absolute rounded-r md:static h-screen md:h-auto w-full md:w-64 hidden md:block text-white text-center bg-white py-2"></div>
+        <div class="flex-auto rounded-r text-black text-center bg-red-700"><img src="1.jpg"></div>
+        <div class="flex-none absolute rounded-r md:static h-screen md:h-auto w-full md:w-64 hidden md:block text-white text-center bg-white py-2"></div>
+</body>-->
+
+ 
+<div class="flex">
+  <div class="w-1/4 bg-white h-12"></div>
+  <div class="w-2/4 bg-gray-400 h-12"> 
+  
+  <img class="w-full" src="1.jpg"">
+
+  <p>hgjhgjkhgjhg</p>
+</div>
+
+  <div class="w-1/4 bg-white h-12"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
